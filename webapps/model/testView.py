31b7b94b009fa4b7d87b7eff055c733f68cfc06a
postersom <poster.som@gmail.com>
2022-12-27 10:50:11 +0700

    Update Script

--- webapps/model/testView.py
+++ webapps/model/testView.py
@@ -1,20 +1,3 @@
-# from marshmallow_sqlalchemy import ModelSchema
-# from model.test import Test
-# from model.status import Status
-# from model.statusView import StatusSchema
-# from marshmallow import fields
-#
-#
-# class TestSchema(ModelSchema):
-#     statuses = fields.Nested(StatusSchema, many=True)
-#
-#     class Meta:
-#         model = Test
-#
-#
-# test_schema = TestSchema()
-
-
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from model.test import Test
 from model.statusView import StatusSchema
